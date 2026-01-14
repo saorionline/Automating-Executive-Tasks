c8a1c7c944d9f42b257e670ba7b718d066ebe192
Saori <saorisabeltovar@gmail.com>
2026-01-14 07:22:44 -0500

    Initializing File again

--- encuesta-saoriux/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-@admin.register(Question)
-
-class QuestionAdmin(admin.ModelAdmin):
-    fields = ('text', 'choice_text', 'date')
-    list_display = ('id', 'text', 'choice_text', 'date')
